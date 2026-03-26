75e392885293460f0e98bf53e5949227f29a7874
Josephat-S <sangwajozaphat@gmail.com>
2026-03-26 19:11:33 +0200

    initial commit

---
+++ src/main/java/com/year2/queryme/QueryMeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.year2.queryme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryMeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueryMeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/year2/queryme/QueryMeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.year2.queryme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryMeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
